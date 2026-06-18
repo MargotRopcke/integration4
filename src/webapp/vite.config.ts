205e0a30965a0537d260176aa4ad507bca4bf6c3
eline <eline.deblancq@student.howest.be>
2026-06-18 12:25:55 +0200

    getting a local link to view on mobile

--- src/webapp/vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [reactRouter()],
-  base: process.env.GITHUB_ACTIONS
-    ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}/`
-    : "/",
-});
